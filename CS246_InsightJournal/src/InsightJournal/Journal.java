60dad563709a08d509602c574bb9a6528d12bf39
DanielRMiller <d.miller1.work@gmail.com>
2014-10-12 22:56:11 -0600

    Changed the termsToFind to a list so that we just have they keys (which is what we are looking for).

--- CS246_InsightJournal/src/InsightJournal/Journal.java
+++ CS246_InsightJournal/src/InsightJournal/Journal.java
@@ -22,14 +22,14 @@ import org.xml.sax.SAXException;
  * @author Xandron
  */
 public class Journal{
-
-   private Map<String, Entry> entries = new TreeMap<>();
-   private String inputFileName;
-   private String outputFileName;
-   private final GUI gui = new GUI();
-   private List<String> booksToFind = new ArrayList<>();
-   private Map<String, List<String>> termsToFind = new HashMap<>();
-   
+    
+    private Map<String, Entry> entries = new TreeMap<>();
+    private String inputFileName;
+    private String outputFileName;
+    private final GUI gui = new GUI();
+    private List<String> booksToFind = new ArrayList<>();
+    private List<String> termsToFind = new ArrayList<>();
+    
     public static void main(String[] args){
         if (args.length == 1) {
             Journal j = new Journal(args[0]);
@@ -37,23 +37,29 @@ public class Journal{
         else
             System.out.println("Cannot find filename");
     }
-
-   Journal(String fileName)
-   {
-      readInputFile(fileName);
-      writeOutputFile();
-      display();
-      PropertiesHandler prop = new PropertiesHandler();
-       try {
-           prop.getPropValues();
-           //System.out.println(PropertiesHandler.scripture);
-           readScriptureFile(PropertiesHandler.scripture);
-           readTermsFile(PropertiesHandler.terms);
-       } catch (IOException ex) {
-           Logger.getLogger(Journal.class.getName()).log(Level.SEVERE, null, ex);
-       }
-   }
-   
+    
+    Journal(String fileName)
+    {
+        // Get everything in from the XML file
+        readInputFile(fileName);
+        writeOutputFile();
+        // displayTest(); // This is to test the XML is loaded (Replace in tests)
+        
+        // Get everything loaded in from the config.properties file.
+        PropertiesHandler prop = new PropertiesHandler();
+        try {
+            prop.getPropValues();
+            //System.out.println(PropertiesHandler.scripture);
+            readScriptureFile(PropertiesHandler.scripture);
+            readTermsFile(PropertiesHandler.terms);
+        } catch (IOException ex) {
+            Logger.getLogger(Journal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        // display desired output for the journal (match terms and scriptures with entries)
+        display();
+    }
+    
     private void readInputFile(String filename) {
         try {
             File xmlFile = new File(filename);
@@ -61,9 +67,9 @@ public class Journal{
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(xmlFile);
             doc.getDocumentElement().normalize();
-           
+            
             System.out.println("Loading file \"" + filename + "\"\n");
-           
+            
             Element rootElement = doc.getDocumentElement();
             entries = parseJournal(rootElement);
             
@@ -71,7 +77,7 @@ public class Journal{
             System.out.println("Sorry, but your file was not able to be parsed.");
         }
     }
-    private Map<String, Entry> parseJournal(Element rootElement){          
+    private Map<String, Entry> parseJournal(Element rootElement){
         Map<String, Entry> entryMap = new HashMap<>();
         NodeList journalNodes = rootElement.getChildNodes();
         for (int i = 0; i < journalNodes.getLength(); i++) {
@@ -112,29 +118,37 @@ public class Journal{
         return rEntry;
     }
     private void writeOutputFile() {}
-
+    
     private Scripture parseScripture(Element rootScriptureElement) {
         Scripture rScripture = new Scripture();
         NamedNodeMap attributes = rootScriptureElement.getAttributes();
         for (int i = 0; i < attributes.getLength(); i++) {
             Node attributeAtI = attributes.item(i);
             switch (attributeAtI.getNodeName()) {
-                    case "book":
-                        rScripture.setBook(attributeAtI.getNodeValue());
-                        break;
-                    case "chapter":
-                        rScripture.setChapter(Integer.parseInt(attributeAtI.getNodeValue()));
-                        break;
-                    case "startverse":
-                        rScripture.setStartVerse(Integer.parseInt(attributeAtI.getNodeValue()));
-                        break;
-                    case "endverse":
-                        rScripture.setEndVerse(Integer.parseInt(attributeAtI.getNodeValue()));
-                        break;
+                case "book":
+                    rScripture.setBook(attributeAtI.getNodeValue());
+                    break;
+                case "chapter":
+                    rScripture.setChapter(Integer.parseInt(attributeAtI.getNodeValue()));
+                    break;
+                case "startverse":
+                    rScripture.setStartVerse(Integer.parseInt(attributeAtI.getNodeValue()));
+                    break;
+                case "endverse":
+                    rScripture.setEndVerse(Integer.parseInt(attributeAtI.getNodeValue()));
+                    break;
             }
         }
         return rScripture;
     }
+    public final void displayTest() {
+        System.out.println("Journal Test Display:");
+        List<String> keys = new ArrayList<>(entries.keySet());
+        for (String key: keys) {
+            System.out.println(entries.get(key).display());
+        }
+    }
+    
     public final void display() {
         System.out.println("Journal Display:");
         List<String> keys = new ArrayList<>(entries.keySet());
@@ -142,7 +156,7 @@ public class Journal{
             System.out.println(entries.get(key).display());
         }
     }
-
+    
     private void readScriptureFile(String scriptureFile) throws IOException{
         FileReader in = new FileReader(scriptureFile);
         BufferedReader br = new BufferedReader(in);
@@ -156,26 +170,16 @@ public class Journal{
         }
         br.close();
     }
-
-    private void readTermsFile(String termsFile) throws IOException{        
+    
+    private void readTermsFile(String termsFile) throws IOException{
         FileReader in = new FileReader(termsFile);
         BufferedReader br = new BufferedReader(in);
+        
         String currentLine = br.readLine();
-        List<String> termLines = new ArrayList<>();
-        List<String> synonyms = new ArrayList<>();
         while(currentLine != null) {
-            termLines.add(currentLine);
+            String term = currentLine.split(":")[0].trim();
+            termsToFind.add(term);
             currentLine = br.readLine();
-        }
-        for (String termLine : termLines){
-            String[] termParts = termLine.split(":");
-            String termKey = termParts[0];
-            String[] temps = termParts[1].split(",");
-            for (String temp : temps) {
-                synonyms.add(temp);
-            }
-            termsToFind.put(termKey, synonyms);
-            synonyms = new ArrayList<>();
         }
         br.close();
     }
